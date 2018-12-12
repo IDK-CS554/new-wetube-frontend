f60de688b39077e9ef5594a2e607e96184d4d8c8
andrewchen <andrew.chen923@gmail.com>
2018-12-12 15:24:32 -0500

    added socket events to login screen

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,73 +1,104 @@
-import React, { Component } from 'react';
-import { Button, Input, Label } from 'reactstrap';
+import React, { Component } from "react";
+import { Button, Input, Label } from "reactstrap";
+import io from "socket.io-client";
 
-class Login extends Component {
+export default class Login extends Component {
+  state = {
+    roomOption: null,
+    username: "",
+    roomId: ""
+  };
 
-	state = {
-		roomOption: null,
-		username: '',
-		roomId: ''
-	};
+  componentDidMount() {
+    const socket = io("http://localhost:5000");
+    this.setState({ socket });
+  }
 
-	isButtonEnabled = () => {
-		const {roomOption, username, roomId} = this.state;
-		if (roomOption === null) {
-			return false;
-		} else if (roomOption === 0) {
-			return username.trim().length !== 0;
-		} else if (roomOption === 1) {
-			return username.trim().length !== 0 && roomId.trim().length !== 0;
-		} else {
-			return false;
-		}
-	};
+  isButtonEnabled = () => {
+    const { roomOption, username, roomId } = this.state;
+    switch (roomOption) {
+      case 0:
+        return username.trim().length;
+      case 1:
+        return username.trim().length && roomId.trim().length;
+      default:
+        return false;
+    }
+  };
 
-	enterRoom = () => {
-		console.log('enter room here');
-	};
+  setRoomOption = option => {
+    this.setState({ roomOption: option });
+  };
 
-	render() {
-		const {roomOption, username, roomId} = this.state;
-		console.log(this.state);
-		return (
-			<div className="login">
+  enterRoom = e => {
+    e.preventDefault();
+    const { username, roomId, roomOption } = this.state;
+    if (roomOption === 1) {
+      this.state.socket.emit("joinRoom", { username, roomId });
+    } else {
+      this.state.socket.emit("createRoom", { username });
+    }
+  };
 
-				<h4 className="header-text">What do you want to do?</h4>
+  render() {
+    const { roomOption, username, roomId } = this.state;
+    return (
+      <form className="login" onSubmit={evt => this.enterRoom(evt)}>
+        <h4 className="header-text">What do you want to do?</h4>
+        <div className="input-section">
+          <Button
+            className="create-room"
+            color="primary"
+            outline={roomOption !== 0}
+            onClick={() => this.setRoomOption(0)}
+          >
+            Create Room
+          </Button>
+          <Button
+            className="join-room"
+            color="primary"
+            outline={roomOption !== 1}
+            onClick={() => this.setRoomOption(1)}
+          >
+            Join Room
+          </Button>
+        </div>
 
-				<div className="input-section">
-					<Button className="create-room" color="primary" outline={roomOption !== 0} onClick={() => this.setState({
-						...this.state,
-						roomOption: 0
-					})}>Create Room</Button>
-					<Button className="join-room" color="primary" outline={roomOption !== 1} onClick={() => this.setState({
-						...this.state,
-						roomOption: 1
-					})}>Join Room</Button>
-				</div>
+        {roomOption !== null && (
+          <div className="input-section">
+            <Label for="username">Username (required):</Label>
+            <Input
+              type="text"
+              id="username"
+              placeholder="Enter Username"
+              value={username}
+              onChange={e => this.setState({ username: e.target.value })}
+            />
+          </div>
+        )}
 
-				{roomOption !== null &&
-				<div className="input-section">
-					<Label for="username">Username (required):</Label>
-					<Input type="text" id="username" placeholder="Enter Username" value={username} onChange={(e) => this.setState({
-						...this.state,
-						username: e.target.value
-					})}/>
-				</div>}
+        {roomOption === 1 && (
+          <div className="input-section">
+            <Label for="room">Room ID (required):</Label>
+            <Input
+              type="text"
+              id="room"
+              placeholder="Enter Room ID"
+              value={roomId}
+              onChange={e => this.setState({ roomId: e.target.value })}
+            />
+          </div>
+        )}
 
-				{roomOption === 1 &&
-				<div className="input-section">
-					<Label for="room">Room ID (required):</Label>
-					<Input type="text" id="room" placeholder="Enter Room ID" value={roomId} onChange={(e) => this.setState({
-						...this.state,
-						roomId: e.target.value
-					})}/>
-				</div>}
-
-				<Button className="submit" color="success" disabled={!this.isButtonEnabled()} onClick={() => this.enterRoom()}>Enter Room</Button>
-
-			</div>
-		)
-	}
+        <Button
+          className="submit"
+          color="success"
+          disabled={!this.isButtonEnabled()}
+          type="submit"
+        >
+          Enter Room
+        </Button>
+      </form>
+    );
+  }
 }
-
-export default Login;
