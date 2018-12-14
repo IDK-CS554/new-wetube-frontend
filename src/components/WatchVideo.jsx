cd34a49159f0f4ac6a332614fc47f4bfc58a531a
andrewchen <andrew.chen923@gmail.com>
2018-12-13 22:17:26 -0500

    choosing a video changes room to a screen with iframe of the selected video

--- src/components/WatchVideo.jsx
+++ src/components/WatchVideo.jsx
@@ -1,13 +1,27 @@
 import React, { Component } from "react";
+import YouTube from "react-youtube";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+
+const mapStateToProps = state => {
+  return {
+    videoId: state.application.videoId
+  };
+};
+
+// const mapDispatchToProps = dispatch => {
+//   return bindActionCreators({ getUsers, changeRoomType }, dispatch);
+// };
 
 export class Watch extends Component {
   render() {
+    const { videoId } = this.props;
     return (
       <div>
-        <h1>Watching Video</h1>
+        <YouTube videoId={videoId} />
       </div>
     );
   }
 }
 
-export default Watch;
+export default connect(mapStateToProps)(Watch);
